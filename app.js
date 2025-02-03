78fc4fb93f27cd3d68b0af242daf37546da440c5
Mykchailo <mishagazin85@gmail.com>
2025-02-03 16:44:32 +0200

    add mongodb

--- app.js
+++ app.js
@@ -4,8 +4,36 @@ const mongoose = require("mongoose");
 const passport = require("./config/passport");
 const authRoutes = require("./routes/auth");
 const MongoStore = require("connect-mongo");
+const {MongoClient, ServerApiVersion} = require("mongodb");
 
 require("dotenv").config();
+
+const DB_URI = 'mongodb+srv://admin:admin@cluster0.8imeo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const dbName = "myDataBaseName";
+
+const client = new MongoClient(DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 
 const app = express();
 app.use(express.json());
